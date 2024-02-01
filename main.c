ab99c4cdf491a14589c6183f38e17ba49248beeb
jardimdanificado <jardeltorreslima01@gmail.com>
2024-02-01 12:07:02 -0300

    some changes

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 #include "stdlib.h"
 #include "string.h"
 
+typedef unsigned char byte;
+
 //vectors   
-typedef struct Vector2
+typedef struct
 {
     int x;
     int y;
-};
+} Vector2;
 #define Vector2(x, y) (Vector2){ x, y }
 
-typedef struct Vector3
+typedef struct
 {
     int x;
     int y;
@@ -35,10 +37,83 @@ typedef struct Vector3
 */
 
 //string
-char* string_concat(char* a, char* b)
+byte* string_concat(char* a, char* b)
 {
     char* result = (char*)malloc(strlen(a) + strlen(b) + 1);
     strcpy(result, a);
     strcat(result, b);
     return(result);
+}
+
+byte*** byteMap3d(int dim1, int dim2, int dim3, char defaultValue) 
+{
+    byte*** matriz3D = (byte ***)malloc(dim1 * sizeof(byte**));
+    // Verificação se a alocação foi bem-sucedida
+    if (matriz3D == NULL) 
+    {
+        perror("Erro ao alocar a primeira dimensão");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < dim1; ++i) 
+    {
+        matriz3D[i] = (byte**)malloc(dim2*sizeof(byte*));
+
+        // Verificação se a alocação foi bem-sucedida
+        if (matriz3D[i] == NULL) 
+        {
+            perror("Erro ao alocar a segunda dimensão");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int j = 0; j < dim2; ++j) 
+        {
+            matriz3D[i][j] = (byte)malloc(dim3);
+
+            // Verificação se a alocação foi bem-sucedida
+            if (matriz3D[i][j] == NULL)
+            {
+                perror("Erro ao alocar a terceira dimensão");
+                exit(EXIT_FAILURE);
+            }
+            for(int k = 0; k < dim3; ++k)
+            {
+                matriz3D[i][j][k] = defaultValue;
+                printf("%d\n", matriz3D[i][j][k]);
+            }
+        }
+    }
+
+    return matriz3D;
+}
+
+void freeByteMap3d(byte*** matriz3D, int dim1, int dim2, int dim3) 
+{
+    for (int x = 0; x < dim1; ++x) 
+    {
+        for (int y = 0; y < dim2; ++y) 
+        {
+            for (int z = 0; z < dim3; ++z) 
+            {
+                free(matriz3D[x][y][z]);
+            }
+            free(matriz3D[x][y]);
+        }
+        free(matriz3D[x]);
+    }
+    free(matriz3D);
+}
+
+
+int main()
+{
+    Vector3 mapSize = Vector3(10, 20, 30);
+    byte*** map = byteMap3d(mapSize.x, mapSize.y, mapSize.z, 0);
+    
+    //string
+    char* result = string_concat("hello", "world");
+    printf("%s\n", result);
+    free(result);
+    freeByteMap3d(map, mapSize.x, mapSize.y, mapSize.z);
+    return 0;
 }
