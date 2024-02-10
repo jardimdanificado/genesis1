2c05029787fe5a3d78049e130e80f26bfd3d249e
jardimdanificado <jardeltorreslima01@gmail.com>
2024-02-10 01:02:39 -0300

    fixed map gen

--- src/main.c
+++ src/main.c
@@ -1,32 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-#define ROWS 64
-#define COLS 64
+#define ROOMS 256
+
+#define ROWSGLOBAL (int)ceil(sqrt(ROOMS))
+#define COLSGLOBAL (int)ceil(sqrt(ROOMS))
+
+#define ROWSLOCAL 64
+#define COLSLOCAL 64
+
+typedef unsigned int byte;
 
 typedef struct 
 {
-    char map[ROWS][COLS];
-} World;
-#define World() {}
+    int x;
+    int y;
+} Vector2;
+#define Vector2(x, y) ((Vector2) {x, y})
+
+typedef struct 
+{
+    int x;
+    int y;
+    int z;
+} Vector3;
+#define Vector3(x, y, z) ((Vector3) {x, y, z})
+
+typedef struct 
+{
+    Vector2 positionLocal;
+    Vector2 positionGlobal;
+    float quality;
+    float weight;    
+} Item;
+
+typedef struct 
+{
+    Vector2 globalPosition;
+    byte map[ROWSLOCAL][COLSLOCAL];
+} Room;
 
 int main(int argc, char *argv[]) 
 {
-    World world = World();
-    if (argc != 2) {
-        printf("Usage: %s <string>\n", argv[0]);
-        return 1;
+    Room rooms[ROOMS]; 
+    for(int i = 0; i < ROOMS; i++)
+    {
+        rooms[i].globalPosition = Vector2(i % ROWSGLOBAL, i / ROWSGLOBAL);
+        for (int j = 0; j < ROWSLOCAL; j++)
+        {
+            for (int k = 0; k < COLSLOCAL; k++)
+            {
+                rooms[i].map[j][k] = 0;
+            }
+        }
     }
-
-    char *str = argv[1];
-    int len = strlen(str);
-    char *rev = malloc(len + 1);
-    for (int i = 0; i < len; i++) {
-        rev[i] = str[len - i - 1];
-    }
-    rev[len] = '\0';
-    printf("%s\n", rev);
-    free(rev);
     return 0;
 }
